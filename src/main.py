8164364400b5086db6b15c183bdfd9b40aaa0ff4
Mahmut Yakan <mahmut_yakan44@hotmail.com>
2020-12-27 14:16:40 +0300

    Refactoring in project

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 import pymongo
 from src import create_collection
 import operator
+import time
 
 
 class Shipment():
@@ -11,8 +12,13 @@ class Shipment():
         self.myclients = pymongo.MongoClient(
             "mongodb+srv://mahmut:123@cluster0.3ycuu.mongodb.net/PorteDB?retryWrites=true&w=majority" )
         self.mydb = self.myclients["PorteDB"]
+
         self.control_dict = {"X": 0, "Y": 0, "Z": 0}
+
         self.output_collection = self.mydb["Output"]
+
+        self.start_time = time.time()
+        self.mydb.Output.create_index([("country_name", 1)] )
 
     def bul(self, item, args):
 
@@ -29,7 +35,6 @@ class Shipment():
                     self.output_collection.insert_one({"FROM_COUNTRY": item, "TO_COUNTRY": j, "SHIPMENT_COMPANY": key, "CARGO_AMONT": value})
                     self.control_dict[key] = self.control_dict[key] + 1
                     break
-
         args.append(item)
 
     @staticmethod
@@ -42,11 +47,6 @@ class Shipment():
         for i in self.mydb.Companies.find({}, {"country_name": 1, "_id": 0}):
             country_list.append(i["country_name"])
         return country_list
-
-    # def inc_processing_function(self, count):
-    #     sum_process = count * (count - 1)
-    #
-    #     return sum_process % 3
 
     def print_output(self):
         for i in self.mydb.Output.find({}, {"_id": 0} ).sort([("FROM_COUNTRY", 1), ("TO_COUNTRY", 1)]):
@@ -66,15 +66,13 @@ if __name__ == '__main__':
     # Burada ülkelerin listesini collectiondan çekiyoruz.
     country_list = shipment.country_function()
 
-    # inc_processing = shipment.inc_processing_function(input_collection.get_document_count)
-
     for i in shipment.mydb.Companies.find({}, {"country_name": 1, "_id": 0}):
 
         shipment.bul(i["country_name"], country_list)
 
     shipment.print_output()
     print(shipment.print_dict)
-
+    print("--- %s seconds ---last" % (time.time() - shipment.start_time) )
     input_collection.drop_collections()
 
     # See PyCharm help at https://www.jetbrains.com/help/pycharm/
